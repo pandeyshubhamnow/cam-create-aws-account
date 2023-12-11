ede6d9ad3f9e68567877f7063f3f57c24dc006a9
pandeyshubhamnow <105665253+pandeyshubhamnow@users.noreply.github.com>
2023-12-11 15:43:55 +0530

    Update awsaccount.tf

--- awsaccount.tf
+++ awsaccount.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
   region     = var.region
 }
 
