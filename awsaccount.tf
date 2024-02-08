42f369ef18f01ea0e4bd6adcf4dcef3e061b46fd
aashrith-snow <159412148+aashrith-snow@users.noreply.github.com>
2024-02-08 11:57:55 +0530

    Update awsaccount.tf

--- awsaccount.tf
+++ awsaccount.tf
@@ -8,15 +8,35 @@ terraform {
 }
 
 provider "aws" {
-  region = var.region
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+  region     = var.region
 }
 
 resource "aws_organizations_account" "account" {
   email = var.root_email
   name = var.account_name
   tags = var.tags
-  close_on_deletion=var.close_delete
-  create_govcloud=var.gov_cloud
+  close_on_deletion = true
 }
 
+resource "aws_budgets_budget" "cost" {
+  name  = join("-", ["SN-CAM-Monthly-Budget", aws_organizations_account.account.id])
+  count = var.monthly_budget > 0 ? 1 : 0
+  budget_type  = "COST"
+  limit_amount = var.monthly_budget
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+  cost_filter {
+    name = "LinkedAccount"
+    values = [
+      aws_organizations_account.account.id
+    ]
+  }
+}
 
+resource "aws_organizations_policy_attachment" "account" {
+  count = var.is_suspended ? 1 : 0
+  policy_id = var.suspend_policy_id
+  target_id = aws_organizations_account.account.id
+}
